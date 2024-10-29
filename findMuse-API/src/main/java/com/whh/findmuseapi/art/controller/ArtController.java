8aa67d433258f02286437958ec2c93fdfea71ddf
kyu-hyun <dlrbgus722@naver.com>
2024-10-30 00:07:08 +0900

    fix : API prefix 수정

--- findMuse-API/src/main/java/com/whh/findmuseapi/art/controller/ArtController.java
+++ findMuse-API/src/main/java/com/whh/findmuseapi/art/controller/ArtController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/v1")
 public class ArtController {
 
     private final ArtService artService;
