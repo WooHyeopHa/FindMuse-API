4420d0fde480bafc427c3d0bdacc6ab9fa2a71c8
kyu-hyun <dlrbgus722@naver.com>
2024-11-19 15:50:14 +0900

    fix : 유저별로 리뷰 목록 조회 오류 로직 수정

--- findMuse-API/src/main/java/com/whh/findmuseapi/review/dto/AllReviewResponse.java
+++ findMuse-API/src/main/java/com/whh/findmuseapi/review/dto/AllReviewResponse.java
@@ -3,14 +3,17 @@ package com.whh.findmuseapi.review.dto;
 import com.whh.findmuseapi.review.entity.ArtReview;
 import com.whh.findmuseapi.review.entity.ArtReviewLike;
 import com.whh.findmuseapi.user.entity.User;
+import jakarta.persistence.Tuple;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Builder
 @Getter
+@Slf4j
 public class AllReviewResponse {
 
     private int reviewCnt;
@@ -18,7 +21,7 @@ public class AllReviewResponse {
     private List<SingleReviewResponse> reviewList;
 
 
-    public static AllReviewResponse toDto(float star, List<ArtReview> reviews) {
+    public static AllReviewResponse toDto(float star, List<Tuple> reviews) {
         return AllReviewResponse.builder()
                 .reviewCnt(reviews.size())
                 .totalStar(star)
@@ -37,10 +40,13 @@ public class AllReviewResponse {
         private boolean isThumbed;
         private int thumbsUpCnt;
 
-        private static List<SingleReviewResponse> toDto(List<ArtReview> reviews) {
+        private static List<SingleReviewResponse> toDto(List<Tuple> reviews) {
             return reviews.stream()
-                    .map(r -> {
+                    .map(tuple -> {
+                        ArtReview r = (ArtReview) tuple.get(0);
+                        ArtReviewLike rl = (ArtReviewLike) tuple.get(1);
                         User writeUser = r.getUser();
+
                         return SingleReviewResponse.builder()
                                 .reviewId(r.getId())
                                 .name(writeUser.getNickname())
@@ -48,7 +54,7 @@ public class AllReviewResponse {
                                 .star(r.getStar())
                                 .content(r.getContent())
                                 .date(r.getCreateDate().format(DateTimeFormatter.ISO_LOCAL_DATE))
-                                .isThumbed(!r.getReviewLikes().isEmpty())
+                                .isThumbed(rl != null)
                                 .thumbsUpCnt(r.getReviewLikes().size()).build();
                     }).toList();
         }
--- findMuse-API/src/main/java/com/whh/findmuseapi/review/repository/ReviewRepository.java
+++ findMuse-API/src/main/java/com/whh/findmuseapi/review/repository/ReviewRepository.java
@@ -3,13 +3,12 @@ package com.whh.findmuseapi.review.repository;
 import com.whh.findmuseapi.art.entity.Art;
 import com.whh.findmuseapi.review.entity.ArtReview;
 import com.whh.findmuseapi.user.entity.User;
+import jakarta.persistence.Tuple;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 @Repository
@@ -18,9 +17,9 @@ public interface ReviewRepository extends JpaRepository<ArtReview, Long> {
     boolean existsByUserAndArt(User user, Art art);
     ArtReview findByUserAndArt(User user, Art art);
 
-    @Query("select ar from ArtReview ar left join ar.reviewLikes arl on ar.id = arl.artReview.id where ar.art.id = :artId and arl.user.id = :userId order by ar.createDate desc")
-    Optional<List<ArtReview>> findAllByArtOrderByCreateDateDesc(Long artId, Long userId);
+    @Query("select ar, arl from ArtReview ar left join ar.reviewLikes arl on arl.user.id = :userId where ar.art.id = :artId order by ar.createDate desc")
+    Optional<List<Tuple>> findAllReviewByUserCreateDateDesc(Long artId, Long userId);
 
-    @Query("select ar from ArtReview ar left join ar.reviewLikes arl on ar.id = arl.artReview.id where ar.art.id = :artId and arl.user.id = :userId order by ar.likeCount desc")
-    Optional<List<ArtReview>> findAllByArtOrderByLikeCountDesc(Long artId, Long userId);
+    @Query("select ar, arl from ArtReview ar left join ar.reviewLikes arl on arl.user.id = :userId where ar.art.id = :artId")
+    Optional<List<Tuple>> findAllReviewByUser(Long artId, Long userId);
 }
