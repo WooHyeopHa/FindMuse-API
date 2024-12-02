f60948615fc3aaddac2671c132709f7459773937
kyu-hyun <dlrbgus722@naver.com>
2024-12-02 16:43:51 +0900

    feat : 북마크 등록 및 해제 기능 (repository)

--- findMuse-API/src/main/java/com/whh/findmuseapi/post/repository/BookmarkRepository.java
+++ findMuse-API/src/main/java/com/whh/findmuseapi/post/repository/BookmarkRepository.java
@@ -6,9 +6,10 @@ import com.whh.findmuseapi.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
     List<Bookmark> findAllByUser(User user);
+    Optional<Bookmark> findByPostAndUser(Post post, User user);
 
-    boolean existsByUserAndPost(User user, Post post);
 }
--- findMuse-API/src/main/java/com/whh/findmuseapi/post/repository/PostRepository.java
+++ findMuse-API/src/main/java/com/whh/findmuseapi/post/repository/PostRepository.java
@@ -1,19 +1,19 @@
 package com.whh.findmuseapi.post.repository;
 
 import com.whh.findmuseapi.post.entity.Post;
-import com.whh.findmuseapi.user.entity.User;
 import jakarta.persistence.LockModeType;
 import jakarta.persistence.QueryHint;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.QueryHints;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @QueryHints({@QueryHint(name = "javax.persistence.lock.timeout",value = "5000")})
-    Optional<Post> findBypostId(Long postId);
+    @Query(value = "select p from Post p where p.id = :postId")
+    Optional<Post> findByIdWithLock(Long postId);
 
     List<Post> findAllByOrderByCreateDateDesc();
 
